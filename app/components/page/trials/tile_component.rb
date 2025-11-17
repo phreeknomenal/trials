4e7df743effd814510db1b0e9216a23378f96817
phreeknomenal <59763494+phreeknomenal@users.noreply.github.com>
2025-11-16 19:15:58 -0600

    Update tile_component.rb

--- app/components/page/trials/tile_component.rb
+++ app/components/page/trials/tile_component.rb
@@ -1,8 +1,8 @@
 class Page::Trials::TileComponent < ApplicationComponent
   erb_template <<-ERB
-    <div class="bg-zinc-50 dark:bg-zinc-800 rounded-lg p-3">
-      <h3 class="text-sm font-medium text-zinc-500 dark:text-zinc-400 mb-1"><%= title %></h3>
-      <p class="text-base font-body font-semibold text-zinc-900 dark:text-zinc-300">
+    <div class="bg-zinc-50 dark:bg-zinc-800 rounded-lg px-5 py-3">
+      <h3 class="text-base font-semibold text-zinc-900 dark:text-zinc-300 mb-1"><%= title %></h3>
+      <p class="text-sm font-body text-zinc-600 dark:text-zinc-400">
         <%= record %>
       </p>
     </div>
